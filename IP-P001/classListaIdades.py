b057efb8bb5efb9c4f40c12ae213e0c7ad990fbd
Matheus Beckerath <matheus.beckerath@hotmail.com>
2023-12-17 23:36:32 -0300

    Modularizando classes

---
+++ IP-P001/classListaIdades.py
@@ -0,0 +1,35 @@
+from classData import Data
+from classAnaliseDados import AnaliseDados
+
+class ListaIdades(AnaliseDados):
+    
+    def __init__(self):
+        super().__init__(type(int))
+        self.__lista = []        
+
+    def entradaDeDados(self, idade):
+        self.__lista.append(idade)
+
+    def mostraMediana(self):
+        self.__lista.sort()
+        mediana = self.__lista[len(self.__lista)//2]
+        print("Mediana:", mediana)
+
+    def mostraMenor(self):
+        menor = min(self.__lista)
+        print("Menor:", menor)
+
+    def mostraMaior(self):
+        maior = max(self.__lista)
+        print("Maior:", maior)
+
+    def listarEmOrdem(self):
+        print("Lista de Idades em Ordem:")
+        for idade in sorted(self.__lista):
+            print(idade)
+
+    def __iter__(self):
+        return iter(self.__lista)
+
+    def __str__(self):
+        return str(self.__lista)
---
+++ IP-P001/classListaSalarios.py
@@ -0,0 +1,35 @@
+from classData import Data
+from classAnaliseDados import AnaliseDados
+
+class ListaSalarios(AnaliseDados):
+
+    def __init__(self):
+        super().__init__(type(float))
+        self.__lista = []        
+
+    def entradaDeDados(self, salario):
+        self.__lista.append(salario)
+
+    def mostraMediana(self):
+        self.__lista.sort()
+        mediana = self.__lista[len(self.__lista)//2]
+        print("Mediana:", mediana)
+
+    def mostraMenor(self):
+        menor = min(self.__lista)
+        print("Menor:", menor)
+
+    def mostraMaior(self):
+        maior = max(self.__lista)
+        print("Maior:", maior)
+
+    def listarEmOrdem(self):
+        print("Lista de Salários em Ordem:")
+        for salario in sorted(self.__lista):
+            print(salario)
+
+    def __iter__(self):
+        return iter(self.__lista)
+
+    def __str__(self):
+        return str(self.__lista)
